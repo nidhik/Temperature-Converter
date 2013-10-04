509ba1c53172d6683fb23c7ff645b93d3b5c5e57
Nidhi Kulkarni <nkulkarni@okta.com>
2013-10-03 22:42:43 -0700

    Initial Commit

---
+++ converter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  converter
+//
+//  Created by Nidhi Kulkarni on 10/3/13.
+//  Copyright (c) 2013 Nidhi Kulkarni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
